2d9cf969b6a7938a9d5d86478c9822d539adf377
Veyrack <veyrack@hotmail.fr>
2019-12-30 22:46:27 +0100

    perf test

---
+++ src/test/GenerateTest.java
@@ -0,0 +1,36 @@
+package test;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class GenerateTest {
+
+	public static void generateFile(int size) {
+		BufferedWriter writer;
+		try {
+			writer = new BufferedWriter(new FileWriter("my_samples/"+size+".points",true));
+		
+		StringBuilder res = new StringBuilder();
+		for(int i = 0; i<size;i++) {
+			int rand = 100 + (int)(Math.random() * 1000);
+			int rand2 = 100 + (int)(Math.random() * 1000);
+			res.append(rand+" "+rand2+"\n");
+		}
+	    writer.write(res.toString());
+	     
+	    writer.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		int i=256;
+		while(i<=425984) {
+			generateFile(i);
+			i+=256;
+		}
+	}
+}
---
+++ src/test/TestTimeCirc.java
@@ -0,0 +1,75 @@
+package test;
+
+import java.awt.Point;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import algorithms.CircMinimum;
+
+public class TestTimeCirc {
+
+	public static void search(final String pattern, final File folder, List<String> result) {
+        for (final File f : folder.listFiles()) {
+
+            if (f.isDirectory()) {
+                search(pattern, f, result);
+            }
+            if (f.isFile()) {
+                if (f.getName().matches(pattern)) {
+                    result.add(f.getAbsolutePath());
+                }
+            }
+
+        }
+    }
+	
+	public static void main(String[] args) {
+		//Test des cercles minimum ainsi que qualité
+		List<Point> points=new ArrayList<>();
+		final File folder = new File("my_samples");
+		
+        List<String> result = new ArrayList<>();
+        
+        search(".*\\.points", folder, result);
+        Collections.sort(result);
+        for (String s : result) {
+        	File file = new File(s); 
+    		BufferedReader br;
+    		try {
+    			br = new BufferedReader(new FileReader(file));
+    			String st;
+    			List<String> l= new ArrayList<>();
+    			while ((st = br.readLine()) != null) {
+    				l = Arrays.asList(st.split(" "));
+    				points.add(new Point(Integer.parseInt(l.get(0)),Integer.parseInt(l.get(1))));
+    				
+    			}
+    			long before = System.currentTimeMillis();
+    			CircMinimum.calculCercleMin((ArrayList<Point>)points);
+    			long after = System.currentTimeMillis();
+    			StringBuilder res = new StringBuilder();
+    			res.append("Pour le fichier "+s+"\n"+(after-before)+"\n");
+    			
+    			BufferedWriter writer = new BufferedWriter(new FileWriter("my_samples/resultatTimeCercle.txt",true));
+    		    writer.write(res.toString());
+    		     
+    		    writer.close();
+    		} 
+    		
+    		catch (FileNotFoundException e) {
+    			e.printStackTrace();
+    		} catch (IOException e) {
+    			e.printStackTrace();
+    		} 
+        }
+	}
+}
